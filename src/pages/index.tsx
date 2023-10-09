3462c6b9127fb847e48b7fb78ad5b5c43b6920b0
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-09 17:41:18 -0300

    Add ConnectModal to home page
    Connect Modal can be used now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,16 @@ import { useState } from 'react';
 
 import { ActionButton } from '@/components/actionButton/ActionButton';
 import { KeyPairModal } from '@/components/keyPairModal/KeyPairModal';
+import { ConnectModal } from '@/components/connectModal/ConnectModal';
+
 import { getRandomKeyPair } from '@/services/stellar';
 
 const Home: NextPage = () => {
   const [privateKey, setPrivateKey] = useState('');
   const [publicKey, setPublicKey] = useState('');
   const [showKeys, setShowKeys] = useState(false);
+  const [showConnectModal, setShowConnectModal] = useState(false);
+  const [isValidKey, setIsValidKey] = useState(true);
 
   function handleGenerateKeys() {
     const keys = getRandomKeyPair();
@@ -17,11 +21,16 @@ const Home: NextPage = () => {
     setShowKeys(true);
   }
 
+  function handleLogIn() {
+    setShowConnectModal(true);
+  }
+
   return (
     <div className="grid h-screen items-center text-center">
       <h1 className="text-6xl text-slate-400">Stellar Wallet</h1>
       <div>
         <ActionButton title="Generate new keys" handleClick={handleGenerateKeys} />
+        <ActionButton title="Connect with secret key" handleClick={() => handleLogIn()} />
       </div>
       {showKeys ? (
         <KeyPairModal
@@ -30,6 +39,17 @@ const Home: NextPage = () => {
           setPrivateKey={setPrivateKey}
           setPublicKey={setPublicKey}
           publicKey={publicKey}
+        />
+      ) : (
+        ''
+      )}
+      {showConnectModal ? (
+        <ConnectModal
+          setShowConnectModal={setShowConnectModal}
+          isValidKey={isValidKey}
+          setIsValidKey={setIsValidKey}
+          privateKey={privateKey}
+          setPrivateKey={setPrivateKey}
         />
       ) : (
         ''
