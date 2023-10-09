83ad2dcca8e99a5e8b5e6286e5af1d009b52a88a
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-09 13:33:45 -0300

    Add ActionButton component
    It will be used in home page

---
+++ src/components/actionButton/ActionButton.tsx
@@ -0,0 +1,17 @@
+export interface ActionButtonProps {
+  title: string;
+  handleClick: () => void;
+}
+
+export function ActionButton({ title, handleClick }: ActionButtonProps) {
+  return (
+    <button
+      className="m-2 rounded border-2 border-solid border-gray-900 bg-transparent px-6 py-3 font-bold text-gray-300 hover:border-gray-600"
+      onClick={() => {
+        handleClick();
+      }}
+    >
+      {title}
+    </button>
+  );
+}
