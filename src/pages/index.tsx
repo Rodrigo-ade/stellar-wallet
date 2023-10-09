4e1e9d8b06ab452ce8827f0ca958723cb7d1e83b
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-09 13:37:22 -0300

    Add generate keys feature
    It will be possible to get new keys

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import { useState } from 'react';
+
+import { ActionButton } from '@/components/actionButton/ActionButton';
+import { KeyPairModal } from '@/components/keyPairModal/KeyPairModal';
+import { getRandomKeyPair } from '@/services/stellar';
 
 const Home: NextPage = () => {
+  const [privateKey, setPrivateKey] = useState('');
+  const [publicKey, setPublicKey] = useState('');
+  const [showKeys, setShowKeys] = useState(false);
+
+  function handleGenerateKeys() {
+    const keys = getRandomKeyPair();
+    setPublicKey(keys.publicKey);
+    setPrivateKey(keys.privateKey);
+    setShowKeys(true);
+  }
+
   return (
-
-      <main className={styles.main}>
-
-      </main>
+    <div className="grid h-screen items-center text-center">
+      <h1 className="text-6xl text-slate-400">Stellar Wallet</h1>
+      <div>
+        <ActionButton title="Generate new keys" handleClick={handleGenerateKeys} />
+      </div>
+      {showKeys ? <KeyPairModal setShowKeys={setShowKeys} privateKey={privateKey} publicKey={publicKey} /> : ''}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
