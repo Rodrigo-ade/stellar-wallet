6ed56adbee39204143cb45f23486415e156c4f45
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-09 13:31:08 -0300

    Add Head
    The whole website will have the same head.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Stellar Wallet</title>
+        <meta name="Create and manage your Stellar account" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
