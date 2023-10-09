eef95bcb0b3d159af5e4d77ce25d78d603953613
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-09 13:23:25 -0300

    Remove unused Home style module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Stellar Wallet</title>
-        <meta name="Create and manage your Stellar account"/>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <main className={styles.main}>
 
