d634d475672ed2f43d44f0c445203b11c2e8e12d
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-09 13:24:46 -0300

    Add custom tailwind colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'purple-dark': '#0b0d12',
+        'purple-highlight': '#292d3e;'
+      }
     },
   },
   plugins: [require('@tailwindcss/forms')],
