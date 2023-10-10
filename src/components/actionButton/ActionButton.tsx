6ace4e6044e144a95d3e3d940fea81878091ca4e
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-10 15:09:03 -0300

    Add ActionButton cyAttribute as optional
    It will be used to test with cypress

--- src/components/actionButton/ActionButton.tsx
+++ src/components/actionButton/ActionButton.tsx
@@ -1,11 +1,13 @@
 export interface ActionButtonProps {
   title: string;
   handleClick: () => void;
+  cyAttribute?: string;
 }
 
-export function ActionButton({ title, handleClick }: ActionButtonProps) {
+export function ActionButton({ title, handleClick, cyAttribute }: ActionButtonProps) {
   return (
     <button
+      data-cy={ cyAttribute ? cyAttribute : '' }
       className="m-2 rounded border-2 border-solid border-gray-900 bg-transparent px-6 py-3 font-bold text-gray-300 hover:border-gray-600"
       onClick={() => {
         handleClick();
