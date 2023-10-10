b081780fff58108ab8979ef25747e8b4b066bf37
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-10 15:10:27 -0300

    Add cypress attribute to multiple elements
    It will be used when testing with cypress

--- src/components/connectModal/ConnectModal.tsx
+++ src/components/connectModal/ConnectModal.tsx
@@ -38,7 +38,7 @@ export function ConnectModal({
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div className="flex min-h-full justify-center p-5 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div className="relative transform overflow-hidden rounded-lg shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg" data-cy="connect-modal">
             <div className="bg-purple-highlight px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <h3 className="text-2xl text-slate-200">Connect with a secret key</h3>
             </div>
@@ -52,13 +52,14 @@ export function ConnectModal({
                   onChange={(e) => {
                     setPrivateKey(e.target.value);
                   }}
+                  data-cy="key-input"
                 />
-                {!isValidKey ? <p className="mt-2 text-xl text-red-500">Invalid secret key</p> : ''}
+                {!isValidKey ? <p className="mt-2 text-xl text-red-500" data-cy="key-error">Invalid secret key</p> : ''}
               </div>
             </form>
             <div className="justify-between bg-purple-highlight	px-4 py-3 sm:flex sm:px-6">
-              <ActionButton title="Connect" handleClick={() => handleConnect()} />
-              <ActionButton title="Close" handleClick={() => handleCloseModal()} />
+              <ActionButton title="Connect" handleClick={() => handleConnect()} cyAttribute="connect-modal-button" />
+              <ActionButton title="Close" handleClick={() => handleCloseModal()} cyAttribute="close-button" />
             </div>
           </div>
         </div>
