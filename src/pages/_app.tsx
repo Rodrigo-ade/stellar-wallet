9daeb95415a9ccb45ae6084e37e200fcad3512c0
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-17 14:21:31 -0300

    Add persistent state Provider to our app
    App Pages can subscribe and update our state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,12 @@ import Head from 'next/head';
 import '../styles/globals.css';
 import '../styles/tailwind.css';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '../storage/stateStorage/stateStorage';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+const persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +17,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="Create and manage your Stellar account" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Component {...pageProps} />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
