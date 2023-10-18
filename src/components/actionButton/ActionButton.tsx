59b06c57a6e6325c97e2027d0de78bfbcff44991
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-18 14:07:12 -0300

    Remove cyAttribute from ActionButton Component
    Instead, a custom class is added.

--- src/components/actionButton/ActionButton.tsx
+++ src/components/actionButton/ActionButton.tsx
@@ -1,13 +1,12 @@
 export interface IActionButtonProps {
   title: string;
   handleClick: () => void;
-  cyAttribute?: string;
 }
 
-export function ActionButton({ title, handleClick, cyAttribute }: IActionButtonProps) {
+export function ActionButton({ title, handleClick }: IActionButtonProps) {
   return (
     <button
-      className={`${cyAttribute} m-2 rounded border-2 border-solid border-gray-900 bg-transparent px-6 py-3 font-bold text-gray-300 hover:border-gray-600`}
+      className={`action-button m-2 rounded border-2 border-solid border-gray-900 bg-transparent px-6 py-3 font-bold text-gray-300 hover:border-gray-600`}
       onClick={() => {
         handleClick();
       }}
