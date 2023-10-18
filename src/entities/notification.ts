5764534ea3f492078cf082b05339b1c9aadcaabd
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-18 16:12:19 -0300

    Remove notification entity

--- src/entities/notification.ts
+++
@@ -1,9 +0,0 @@
-export class Notification {
-  isSuccess: boolean;
-  message: string;
-
-  constructor(isSuccess: boolean, message: string) {
-    this.isSuccess = isSuccess;
-    this.message = message;
-  }
-}
