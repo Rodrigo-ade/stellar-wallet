174453227cec71935c36ee06b18fb28fef24df08
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-18 08:29:28 -0300

    Remove notification and it's action from state
    Notification will be handled based on accountData attribute

--- src/storage/stateStorage/stateSlice.ts
+++ src/storage/stateStorage/stateSlice.ts
@@ -1,18 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { Notification } from '@/entities/notification';
 import { AccountData } from '@/entities/accountData';
 
 interface IStorageAccountStateProps {
   accountData: AccountData | null;
   loading: boolean;
-  notification: Notification | null;
 }
 
 const initialState: IStorageAccountStateProps = {
   accountData: null,
   loading: false,
-  notification: null,
 };
 
 const stateSlice = createSlice({
@@ -28,15 +25,11 @@ const stateSlice = createSlice({
     },
     loading: (state) => {
       return { ...state, loading: true, accountData: null, notification: null };
-    },
-    notify: (state, action) => {
-      const { isSuccess, message } = action.payload;
-      return { ...state, loading: false, notification: { isSuccess, message } };
-    },
+    }
   },
 });
 
-export const { logIn, logOut, loading, notify } = stateSlice.actions;
+export const { logIn, logOut, loading } = stateSlice.actions;
 
 interface IAccountStateProps {
   accountState: IStorageAccountStateProps;
@@ -44,6 +37,5 @@ interface IAccountStateProps {
 
 export const selectAccountData = (state: IAccountStateProps) => state.accountState.accountData;
 export const selectLoading = (state: IAccountStateProps) => state.accountState.loading;
-export const selectNotification = (state: IAccountStateProps) => state.accountState.notification;
 
 export default stateSlice.reducer;
