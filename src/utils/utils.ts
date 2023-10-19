41d3d1087f3610edb10f9ffa49e4e44ab3fc2cee
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-19 09:20:38 -0300

    Refactor redirectToDashboard function
    Dynamic parameters won't be used

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,7 @@
 import Router from 'next/router';
 
-export function redirectToDashboard(publicKey: string) {
-  Router.push({ pathname: '/dashboard', query: { account: publicKey } });
+export function redirectToDashboard() {
+  Router.push({ pathname: '/dashboard' });
+}
+
 }
