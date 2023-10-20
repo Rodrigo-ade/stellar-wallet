47012aeaee31acc665a3dfac096a72ef942eeed9
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-20 09:48:57 -0300

    Add Disconnect option to Navbar component

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,19 @@
-export function Navbar() {
+import { ActionButton } from '../actionButton/ActionButton';
+
+export interface INavbarProps {
+  handleDisconnect: () => void;
+}
+
+export function Navbar({ handleDisconnect }: INavbarProps) {
   return (
     <nav className="flex h-40 justify-between bg-purple-slight-dark p-10 text-slate-400">
       <div>
         <p className="text-xl font-medium">
           Stellar <span className="rounded-md bg-violet-strong p-1 text-xs text-white">Account Viewer</span>
         </p>
+      </div>
+      <div>
+        <ActionButton title="Disconnect" handleClick={() => handleDisconnect()} />
       </div>
     </nav>
   );
