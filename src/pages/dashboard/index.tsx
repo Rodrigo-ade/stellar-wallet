de7411cddd0c9c7017e8de9112d34e9c245e0c4f
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-20 12:31:30 -0300

    Add Loading component to Dashboard page
    It will render while data fetching

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectAccount } from '@/storage/stateStorage/stateSlice';
 
 import { NotificationModal } from '@/components/notificationModal/NotificationModal';
 import { Navbar } from '@/components/navbar/Navbar';
+import { Loading } from '@/components/loading/Loading';
 
 import { redirectToIndex } from '@/utils/utils';
 import { logOut } from '@/storage/stateStorage/stateSlice';
 
 export default function Dashboard() {
+  const [loading, setLoading] = useState(true);
   const account = useSelector(selectAccount);
-  
+
   const dispatch = useDispatch();
 
   function handleDisconnect() {
@@ -24,17 +27,24 @@ export default function Dashboard() {
       />
     );
   } else {
-      return (
-        <>
-          <Navbar handleDisconnect={handleDisconnect} />
-          <div className="h-screen px-10 pt-5">
-            <div>
-              <p className="mb-3 text-3xl text-slate-200">Your Stellar Public Key</p>
-              <p className="break-words text-3xl font-semibold text-slate-400">{account.id}</p>
-            </div>
-            <hr className=" m-5 border-violet-strong" />
+    return (
+      <>
+        <Navbar handleDisconnect={handleDisconnect} />
+        <div className="h-screen px-10 pt-5">
+          <div>
+            <p className="mb-3 text-3xl text-slate-200">Your Stellar Public Key</p>
+            <p className="break-words text-3xl font-semibold text-slate-400">{account.id}</p>
           </div>
-        </>
-      );
-    }
+          <hr className=" m-5 border-violet-strong" />
+          {loading ? (
+            <div className="p-14">
+              <Loading title="Loading your account information..." />
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+      </>
+    );
+  }
 }
