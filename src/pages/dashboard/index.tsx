a59433b1010a59a0f61f99f6ad17c0cba86bb7e8
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-20 10:41:05 -0300

    Add conditional rendering to Dashboard page
    It will notify and redirect when user is not logged in.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,20 +1,31 @@
 import { useSelector } from 'react-redux';
 import { selectAccount } from '@/storage/stateStorage/stateSlice';
 
+import { NotificationModal } from '@/components/notificationModal/NotificationModal';
+
+import { redirectToIndex } from '@/utils/utils';
+
 export default function Dashboard() {
   const account = useSelector(selectAccount);
 
-  if(account){
+  if (!account) {
     return (
-      <>
-        <div className="h-screen px-10 pt-5">
-          <div>
-            <p className="mb-3 text-3xl text-slate-200">Your Stellar Public Key</p>
-            <p className="break-words text-3xl font-semibold text-slate-400">{account.id}</p>
-          </div>
-          <hr className=" m-5 border-violet-strong" />
-        </div>
-      </>
+      <NotificationModal
+        notification={{ isSuccess: false, message: 'Not logged In, you will be redirected...' }}
+        redirect={redirectToIndex}
+      />
     );
-  }
+  } else {
+      return (
+        <>
+          <div className="h-screen px-10 pt-5">
+            <div>
+              <p className="mb-3 text-3xl text-slate-200">Your Stellar Public Key</p>
+              <p className="break-words text-3xl font-semibold text-slate-400">{account.id}</p>
+            </div>
+            <hr className=" m-5 border-violet-strong" />
+          </div>
+        </>
+      );
+    }
 }
