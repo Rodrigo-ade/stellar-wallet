348c57651e0d23e43ad7359234b3dd0ebf193fed
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-20 09:41:57 -0300

    Add Loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,10 @@
+export function Loading() {
+  return (
+    <>
+      <div className="m-5 grid justify-center text-center">
+        <div className="h-28 w-28 animate-spin rounded-full border-8 border-solid border-purple-500 border-t-transparent"></div>
+      </div>
+      <p className="mt-2 text-center text-2xl font-semibold text-slate-300">Loading...</p>
+    </>
+  );
+}
