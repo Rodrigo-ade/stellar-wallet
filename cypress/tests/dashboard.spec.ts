5c4b96b6f9ec556f8866f382ae6282132c11816b
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-23 18:24:08 -0300

    Add Dashboard tests for Wealthy account

--- cypress/tests/dashboard.spec.ts
+++ cypress/tests/dashboard.spec.ts
@@ -9,3 +9,25 @@ const connect = (key: string) => {
     cy.get('.key-input').type(key);
     cy.get('.action-button').eq(2).click();
 }
+
+context('Dashboard', () => {
+  describe('Wealthy Account', () => {
+    before(() => {
+      connect(WEALTHY_ACCOUNT_SECRET_KEY);
+      cy.wait(750);
+    });
+    
+    it('Should have 10000.00 as balance', () => {
+      cy.get('.balance-0').should('have.text', '10000.00 Lumens (XLM)');
+    });
+
+    it('Should not exist "Fund Account" button', () =>  {
+      cy.contains('Fund Account').should('not.exist');
+    });
+
+    it('Should not exist "Inactive Account" panel', () => {
+      cy.get('.inactive-account').should('not.exist');
+    })
+
+  });
+});
