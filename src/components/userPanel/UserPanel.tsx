e03451f471539e4fd99dd528166a7f04417f5529
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-23 18:19:33 -0300

    Add class to each balance in UserPanel

--- src/components/userPanel/UserPanel.tsx
+++ src/components/userPanel/UserPanel.tsx
@@ -18,7 +18,7 @@ export function UserPanel({ fundAccount, balance, publicKey, setFunded }: IUserP
   const [accountExists, setAccountExists] = useState(true);
   const [notification, setNotification] = useState<Notification | null>(null);
 
-  const listedBalance = balance?.map((tempBalance) => {
+  const listedBalance = balance?.map((tempBalance, index) => {
     const balance = Number(tempBalance.balance).toFixed(2);
     const asset = tempBalance.asset === 'native' ? 'Lumens (XLM)' : tempBalance.asset;
 
@@ -27,7 +27,7 @@ export function UserPanel({ fundAccount, balance, publicKey, setFunded }: IUserP
     }
 
     return (
-      <p className="text-3xl font-semibold text-slate-200" key={asset}>
+      <p className={ `balance-${index} text-3xl font-semibold text-slate-200`} key={asset}>
         {balance} {asset}
       </p>
     );
