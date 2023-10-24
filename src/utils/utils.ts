ba4b759688d4aa4653c5b2dab62d6d3a552378fd
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-24 17:05:39 -0300

    Add getAccount function
    getAccount waits data fetching, then modifies it.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import Router from 'next/router';
+import { getAccount as getAccountService } from '@/services/stellar';
 
 export interface IBalance {
   asset: string;
@@ -20,4 +21,37 @@ export function redirectToDashboard() {
 
 export function redirectToIndex() {
   Router.push({ pathname: '/' });
+}
+
+export async function getAccount(publicKey: string) {
+  let payments: IPayment[];
+  let balance: IBalance[];
+
+  const account = await getAccountService(publicKey);
+
+  if(account instanceof Error){
+    payments = [];
+    balance = [
+      {
+        asset: 'XLM',
+        balance: '0',
+      },
+    ];
+  }else {
+    balance = account.balances.filter((tempB) => Number(tempB.balance) > 0).map((tempBalance) => ({
+      asset: tempBalance.asset_type === 'native' ? 'XLM' : tempBalance.asset_code, 
+      balance: tempBalance.balance,
+    }));
+
+    payments = (await account.payments({order:'desc'})).records.map((tempPayment) => ({
+      ammount: tempPayment.amount,
+      type: tempPayment.type.toString(),
+      date: tempPayment.created_at,
+      from: tempPayment.from,
+      to: tempPayment.to,
+      asset_code: tempPayment.asset_code,
+    }));
+  }
+
+  return { payments, balance };
 }
