2bba7e893679cdb404427261ca8c48706af3b178
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-25 08:32:37 -0300

    Add payments list to UserPanel

--- src/components/userPanel/UserPanel.tsx
+++ src/components/userPanel/UserPanel.tsx
@@ -3,20 +3,33 @@ import { useState } from 'react';
 import { ActionButton } from '../actionButton/ActionButton';
 import { NotificationModal } from '../notificationModal/NotificationModal';
 
-import { IBalance } from '@/services/stellar';
-
 import { Notification } from '@/entities/Notification';
+import { IPayment, IBalance } from '@/utils/utils';
 
 interface IUserPanelProps {
   publicKey: string;
   fundAccount: (publicKey: string) => Promise<boolean>;
   balance: IBalance[] | null;
+  payments: IPayment[] | null;
   setFunded: (isFunding: boolean) => void;
 }
 
-export function UserPanel({ fundAccount, balance, publicKey, setFunded }: IUserPanelProps): React.ReactElement {
+export function UserPanel({ fundAccount, balance, payments, publicKey, setFunded }: IUserPanelProps): React.ReactElement {
   const [accountExists, setAccountExists] = useState(true);
   const [notification, setNotification] = useState<Notification | null>(null);
+
+  const listedPayments = payments?.map((tempPayment, index) => {
+    return (
+      <div key={`payment-${index}`} className='text-white m-3 text-center border-violet-strong bg-violet-strong bg-opacity-20 p-1 justify-center break-words'>
+        <p>
+          <span className='text-slate-400'>{tempPayment.type}</span>: 
+          (<span className='text-yellow-400'>{tempPayment.asset_code}</span> - <span className='text-green-400'> {tempPayment.ammount}</span>)
+        </p>
+        <p><span className='text-slate-400'>To:</span> {tempPayment.to}</p>
+        <span className='text-slate-400'> Date:</span> {tempPayment.date.split('T')[0]}
+      </div>
+    );
+  });
 
   const listedBalance = balance?.map((tempBalance, index) => {
     const balance = Number(tempBalance.balance).toFixed(2);
@@ -90,6 +103,10 @@ export function UserPanel({ fundAccount, balance, publicKey, setFunded }: IUserP
           ></ActionButton>
         </div>
       )}
+      <div className='mt-3'>
+        <p className="mb-3 text-3xl text-slate-200">Your Payments</p>
+        {listedPayments && listedPayments?.length > 0 ? listedPayments : <p className='text-slate-400 text-xl'>No Payments found...</p>}
+      </div>
     </>
   );
 }
