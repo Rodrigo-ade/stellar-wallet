1cf16c361b53428a68c00ebe20b3c2127c04f755
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-27 13:46:30 -0300

    Add test for Payment with funded account

--- cypress/tests/payments.spec.ts
+++ cypress/tests/payments.spec.ts
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 const DEFAULT_URL = Cypress.env('BASE_URL');
+const FUNDED_ACCOUNT_SECRET_KEY = Cypress.env('FUNDED_ACCOUNT_SECRET_KEY');
 const NEW_ACCOUNT_SECRET_KEY = Cypress.env('NEW_ACCOUNT_SECRET_KEY');
+const WRONG_PUBLIC_KEY_MESSAGE = 'Invalid public key';
 const TIMEOUT_MS = 1500;
 
 const connect = (key: string) => {
@@ -11,6 +13,19 @@ const connect = (key: string) => {
 };
 
 context('Payments', () => {
+  describe('Funded Account', () => {
+    before(() => {
+      connect(FUNDED_ACCOUNT_SECRET_KEY);
+      cy.wait(TIMEOUT_MS);
+    });
+
+    it('Should not send Payment with wrong data', () => {
+      cy.get('.sender-private-key').type(FUNDED_ACCOUNT_SECRET_KEY);
+      cy.contains('Send').click();
+      cy.get('.notification').should('have.text', WRONG_PUBLIC_KEY_MESSAGE);
+    });
+  });
+
   describe('New Account', () => {
     before(() => {
       connect(NEW_ACCOUNT_SECRET_KEY);
