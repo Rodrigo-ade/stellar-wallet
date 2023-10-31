f0008d8b9a2d2c0c7a08944fef20a396ea927dfb
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-10-31 12:01:17 -0300

    Fix typos in albedo tests

--- cypress/tests/albedo.spec.ts
+++ cypress/tests/albedo.spec.ts
@@ -1,14 +1,14 @@
 /// <reference types="cypress" />
 
-const DEFAULT_URL = Cypress.env('BASE_URL');
+const BASE_URL = Cypress.env('BASE_URL');
 const FUNDED_ACCOUNT_SECRET_KEY = Cypress.env('FUNDED_ACCOUNT_SECRET_KEY');
 const RECEIVER_PUBLIC_KEY = Cypress.env('RECEIVER_PUBLIC_KEY');
 const BALANCE_TO_SEND = '3';
-const WRONG_VALUE = 'AFAFFAFAFA';
+const WRONG_RECEIVER_PUBLIC_KEY = 'AFAFFAFAFA';
 const TIMEOUT_MS = 3000;
 
 const connect = (key: string) => {
-  cy.visit(DEFAULT_URL);
+  cy.visit(BASE_URL);
   cy.get('.action-button').eq(1).click();
   cy.get('.key-input').type(key);
   cy.contains(/^Connect$/).click();
@@ -23,14 +23,14 @@ const stubAlbedoWindow = () => {
 context('Albedo', () => {
   describe('Log In', () => {
     before(() => {
-      cy.visit(DEFAULT_URL);
+      cy.visit(BASE_URL);
     });
 
     beforeEach(() => {
       cy.contains('Connect with Albedo').as('albedoButton');
     });
 
-    it('Should exist Albedo Button', () => {
+    it('Should Albedo Button exist', () => {
       cy.get('@albedoButton').should('exist');
     });
 
@@ -57,7 +57,7 @@ context('Albedo', () => {
     });
 
     it('Should show Albedo error using wrong values', () => {
-      cy.get('@receiverPublicKey').type(WRONG_VALUE);
+      cy.get('@receiverPublicKey').type(WRONG_RECEIVER_PUBLIC_KEY);
       cy.get('@albedoSendButton').click();
       cy.get('.notification').should('have.text', 'Intent request is invalid.');
     });
